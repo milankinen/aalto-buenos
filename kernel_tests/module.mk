0bcb2ba031bd2f3a404ddeba6b2f52ba1e1e4c38
Hujanen Jaakko <jaakko.hujanen@aalto.fi>
2013-02-11 12:21:54 +0200

    Minor corrections to make compilation work.

--- kernel_tests/module.mk
+++ kernel_tests/module.mk
@@ -4,11 +4,7 @@
 MODULE := kernel_tests
 
 
-<<<<<<< HEAD
-FILES := makeWater.c lock_tests.c cond_tests.c
-=======
-FILES := make_water.c lock_tests.c cond_tests.c
->>>>>>> 7cee4311fa57dae7867e2e3b616733a08958b146
+FILES := make_water.c lock_tests.c cond_tests.c thread_sleep_tests.c
 
 SRC += $(patsubst %, $(MODULE)/%, $(FILES))
 

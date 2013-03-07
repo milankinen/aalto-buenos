58d7806771d56913f1fa6a22bf75e680f4ac4da9
Hujanen Jaakko <jaakko.hujanen@aalto.fi>
2013-03-07 19:43:06 +0200

    more meaty shell but still broken

--- tests/shell.c
+++ tests/shell.c
@@ -9,16 +9,20 @@
 #define SHELL_ARGLEN 16
 #define SHELL_MAXARGS 16
 
+/* +2 to accommodate program name and null
+ * +1 to accommodate null
+ */
+#define SHELL_ARGUMENT_ARRAY_LEN    SHELL_MAXARGS+2
+#define SHELL_ARGUMENT_STRING_LEN   SHELL_ARGLEN+1
+
 /* data structure to hold the command and its arguments in c like format
  */
 typedef struct {
     /* number of arguments */
-    int c;
+    int argc;
     /* null ended strings in null ended array
-     * +2 to accommodate program name and null
-     * +1 to accommodate null
      */
-    char v[SHELL_MAXARGS+2][SHELL_ARGLEN+1];
+    char argv[SHELL_ARGUMENT_ARRAY_LEN][SHELL_ARGUMENT_STRING_LEN];
 } shell_cmd;
 
 /* data structure to hold shell status
@@ -26,8 +30,17 @@ typedef struct {
  * foreground to indicate if a process was started on foreground
  *  background
  */
+typedef enum error_e {
+    OK,
+    READFAIL,
+    EXECPFAIL,
+    ARGNFAIL,
+    ARGLENFAIL,
+    FATAL
+} error_t;
+
 typedef struct {
-    int error;
+    error_t error;
     int foreground;
 } shell_status;
 
@@ -47,7 +60,8 @@ shell_prompt(const char *str) {
 
 /* readline: reads line character by character until line break
  * returns pointer to the end of the read buffer i.e. the \0 character
- * in error situation 0 is returned
+ * in error situation 0 is returned if only end of line is read the
+ * same address buf is returned
  */
 char *
 shell_readline(char *buf) {
@@ -74,39 +88,123 @@ shell_readline(char *buf) {
     return buf;
 }
 
+int
+shell_iswhitespace(char c) {
+    if (c == ' ' || c == '\n' || c == '\t' || c == '\0')
+        return 1;
+    else
+        return 0;
+}
+
+/* next whitespace returns pointer to the next whitespace character
+ * in a string until end ptr
+ * if end is encountered return it
+ */
+char *
+shell_next_whitespace(char *str, char *end) {
+    while (shell_iswhitespace(*str) && (str < end))
+        str++;
+    return str;
+}
+
+/* next non-white returns pointer to the next non-white space
+ * character in a string until end
+ * if end is encountered return it
+ */
+char *
+shell_next_nonwhite(char *str, char *end) {
+    while (!shell_iswhitespace(*str) && (str < end))
+        str++;
+    return str;
+}
+
 /* parse: reads line with shell_readline and builds
  * shell_cmd and shell_status data structures
+ * - sets shell_cmd null if end of file was the only thing read
+ * - sets the fields of shell_cmd null if no command was read
  */
 void
 shell_parse(shell_cmd *cmd, shell_status *status) {
     char cmdln[SHELL_LINELEN];
-    char *end;
-    end = shell_readline(cmdln);
+    char *end = shell_readline(cmdln);
+    char *next_white, *next_nonwhite;
+    /* check end for errors */
+    if (end == 0) {
+        /* read failed */
+        status->error = READFAIL;
+        return;
+    }
+    else
+        status->error = OK;
+    /* check if EOF was encountered */
+    if (cmdln-end == 0) {
+        cmd = 0;
+        return;
+    }
     /* partition cmdln to c strigs separated by whitespace and
      * assign them to shell_cmd data structure
+     *
+     * start by entering arguments in their positions
      */
-    /* TODO, remember to move the following :) */
-    end = end;
-    cmd = cmd;
-    status = status;
+    int i,j;
+    next_nonwhite = shell_next_nonwhite(cmdln, end);
+    for (i = 0; i < SHELL_ARGUMENT_ARRAY_LEN; i++) {
+        next_white = shell_next_whitespace(next_nonwhite, end);
+        /* copy the string between next_nonwhite and next_white
+         * to its slot in the shell_cmd if arg is too long
+         * update error field in status
+         */
+        for (j = 0; next_nonwhite + j < next_white; j++) {
+            if (j >= SHELL_ARGUMENT_STRING_LEN) {
+                status->error = ARGLENFAIL;
+                cmd->argv[i][j-1] = '\0';
+                goto break_both_loops;
+            }
+            cmd->argv[i][j] = next_nonwhite[j];
+        }
+        next_nonwhite = shell_next_nonwhite(next_white, end);
+        if (next_nonwhite == end)
+            break;
+    }
+    /* check if all array was used
+     * this should not happen if there was the allowed number
+     * of arguments
+     */
+    if (i == SHELL_ARGUMENT_ARRAY_LEN) {
+        status->error = ARGNFAIL;
+        /* the array is not null ended any more
+         * correct that for robustness
+         */
+        cmd->argv[i-1] = 0; /* FIXME */
+        return;
+    }
+    /* now all arguments are in place 
+     * make the array null ended and update argc
+     */
+break_both_loops:
+    cmd->argv[i] = 0; /* FIXME */
+    cmd->argc = i;
+    return;
 }
 
 /* stop: return true when the command is exit or EOF
  */
 int
-shell_stop(shell_cmd *cmd) {
-    /* TODO */
-    cmd = cmd;
-    return 1;
+shell_stop(shell_cmd *cmd, shell_status *status) {
+    /* cmd 0 if only end of file was read
+     * this is when we stop
+     */
+    if (cmd && (status->error != FATAL))
+        return 0;
+    else
+        return 1;
 }
 
 /* execute: call execp and return pid of the created process
  */
 int
 shell_execute(shell_cmd *cmd) {
-    /* TODO */
-    cmd = cmd;
-    return 0;
+    return syscall_execp(cmd->argv[0], cmd->argc, cmd->argv); /* FIXME */
 }
 
 /* foreground: return true if the shell was started in foreground
@@ -127,6 +225,35 @@ shell_print_int(int pid) {
     shell_print_str(pidstr);
 }
 
+void
+shell_handle_error(shell_status *status) {
+    switch(status->error) {
+        case READFAIL:
+            shell_print_str("Error: syscall_read failed.\n");
+            /* shut down the shell in the next phase */
+            status->error = FATAL;
+            break;
+        case ARGNFAIL:
+            shell_print_str("Error: too many arguments.\n");
+            /* non fatal error, return ok state */
+            status->error = OK;
+            break;
+        case ARGLENFAIL:
+            shell_print_str("Error: too long arguments.\n");
+            /* non fatal error, return ok state */
+            status->error = OK;
+            break;
+        case EXECPFAIL:
+            shell_print_str("Error: execution failed.\n");
+            /* non fatal error, return ok state */
+            status->error = OK;
+            break;
+        default:
+            shell_print_str("Error: n/a.\n");
+            status->error = FATAL;
+    }
+}
+
 int
 main(void) {
     int pid;
@@ -141,11 +268,17 @@ main(void) {
     while (1) {
         shell_prompt("> ");
         shell_parse(cmd,status);
-        /* TODO: error handling of read line fail */
-        if (shell_stop(cmd))
+        if (status->error != OK) {
+            shell_handle_error(status);
+            continue;
+        }
+        if (shell_stop(cmd, status))
             break;
         pid = shell_execute(cmd);
-        /* TODO: error handling of execp fail */
+        if (status->error != OK) {
+            shell_handle_error(status);
+            continue;
+        }
         if (shell_foreground(status)) {
             syscall_join(pid);
         }
